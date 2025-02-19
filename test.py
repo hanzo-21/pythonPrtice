7ac5df6bc36cc6b1a69fae0a8392f97ebb2432c1
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-19 19:41:06 +0545

    initialcommit

---
+++ test.py
@@ -0,0 +1,11 @@
+# Replace ___ with your code below
+
+# get integer input
+n = int(input())
+
+# iterate the outer loop n times
+for num in range(n):
+    # iterate the inner loop 2 times
+    for num in range(2):
+        # print "*"
+        print("*")
