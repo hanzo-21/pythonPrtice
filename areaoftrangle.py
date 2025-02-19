da32608f4caf85b4f52458079965cf87d86b3680
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-19 19:55:53 +0545

    area of traongel

---
+++ areaoftrangle.py
@@ -0,0 +1,11 @@
+# Replace ___ with your code
+
+# take input for base and height
+base  = float(input())
+height = float(input())
+
+# calculate the area
+area = (base*height)/2
+
+# print area
+print(area)
