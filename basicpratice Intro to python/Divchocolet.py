e5e08a317d98f37ba2522b6de797185cb4da6679
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-19 20:03:06 +0545

    finished into to python pratice

---
+++ basicpratice Intro to python/Divchocolet.py
@@ -0,0 +1,12 @@
+# Replace ___ with your code
+
+# take two integer inputs for 
+chocolates= int(input())
+children= int(input())
+
+
+# find chocolates each children will get and and print it
+print(int(chocolates/children))
+
+# find remaining chocolates and print it
+print(int(chocolates%children))
---
+++ basicpratice Intro to python/avgNum.py
@@ -0,0 +1,13 @@
+# Replace ___ with your code
+
+# take three float input for 
+number1= float(input())
+number2 = float(input())
+number3 = float(input())
+
+
+# find the average using formula
+avg  = (number1+number2+number3)/3
+
+# print calculated average
+print(avg)
