a6807f0f9ed3472f3ed193edeecc73e4cc0b5f9f
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-19 19:53:03 +0545

    si

---
+++ simpleinterst.py
@@ -0,0 +1,16 @@
+# Replace ___ with your code
+
+# take float input for 
+principal = float(input())
+rate= float(input())
+time= float(input())
+
+
+# calculate the simple interest
+si = (principal * rate * time) /100
+
+# calculate the final amount
+final = principal+ si
+
+# print interest and total_sum in separate lines
+print(final)
