238c1ff36e8e0bb2b4c51086b637df7b7d916a41
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-26 12:20:21 +0545

    arange()

---
+++ python Data Science/numpyArrange.py
@@ -0,0 +1,11 @@
+# Replace ___ with your code
+import numpy as np
+# take user inputs
+start =  int(input())
+stop =  int(input())
+# use arange() to create an array of even numbers between start and stop
+even_array = np.arange(start,stop,2)
+
+
+# display the resulting array
+print(even_array)
