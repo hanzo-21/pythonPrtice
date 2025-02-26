47a916b817d374acf894ed847416a0e4dc0ff292
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-26 10:30:18 +0545

    list revision

---
+++ python Data Science/silicingList.py
@@ -0,0 +1,18 @@
+# Replace ___ with your code
+
+# take temperature log as user input
+temperatures = list(map(int, input().split()))
+
+# retrieve the temperature of the first day using index number
+print(temperatures[0])
+
+# retrieve the temperature of the last day using negative indexing
+print(temperatures[-1])
+
+# retrieve the temperatures for the second week using slicing
+print(temperatures[7:7*2+1])
+
+# reverse the order of temperatures in the list using slicing
+
+print(temperatures[::-1])
+# print the result
