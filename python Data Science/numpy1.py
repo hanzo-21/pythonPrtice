228e9c659683b386511562440141eb086537cb43
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-26 10:35:14 +0545

    beging of numpy

---
+++ python Data Science/numpy1.py
@@ -0,0 +1,10 @@
+# Replace ___ with your code
+import numpy as np
+
+# split user input into a list
+py_list = list(input().split())
+
+# create numpy array using list
+np_array = np.array(py_list)
+
+print(np_array)
