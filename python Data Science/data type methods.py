0b0f4bbae5afd2a09d56254be98cd724d785bde3
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-27 07:56:52 +0545

    data type methods

---
+++ python Data Science/data type methods.py
@@ -0,0 +1,11 @@
+# Replace ___ with your code
+import numpy as np
+
+# create a numpy array
+numbers = np.array([3.3, 4.1, 5.0, 8.4, 10.1, 12.0])
+
+# use dtype to find the data type of an array
+print(numbers.dtype)
+
+# use size to find the total number of elements in an array
+print(numbers.size)
