aaa9853e82f50a36d3239c6b1fcd5952249b5101
aayush rana magar <realnaramagaraayush@gmail.com>
2025-02-28 18:13:06 +0545

    basic py function

---
+++ python recursion/basic recursion.py
@@ -0,0 +1,15 @@
+# Replace ___ with your code
+
+# create the calculate_average() function
+def calculate_average(x,y,z):
+    return (x+y+z)/3
+
+# take user input
+mark1 = int(input())
+mark2 = int(input())
+mark3 = int(input())
+
+# call the function with arguments mark1, mark2, and mark3
+average_marks = calculate_average(mark1,mark2,mark3)
+
+print(average_marks)
