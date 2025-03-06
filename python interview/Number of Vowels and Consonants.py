e0dc473de2bb26ac021e267a6afa092b05b55637
aayush rana magar <realnaramagaraayush@gmail.com>
2025-03-06 19:09:07 +0545

    it counts the number of vowels and consonetnst in

---
+++ python interview/Number of Vowels and Consonants.py
@@ -0,0 +1,35 @@
+# replace ___ with your code
+
+# define a function to count vowels and consonants in a string
+def count_vowels_and_consonants(string):
+
+    # define a list of vowels to be removed
+    vowels = 'aeiouAEIOU'
+
+    # initialize counters for vowels and consonants
+    vowel_count = 0
+    consonant_count = 0
+
+    # loop through each character in the string
+    for char in string:
+
+        # if the character is a vowel, increment the vowel counter
+        if vowels.__contains__(char):
+            vowel_count += 1
+
+        # if the character is a space, continue to the next character
+        elif char==" ":
+            continue
+
+        # else, increment the consonant counter
+        else:
+            consonant_count+=1
+
+    # print vowels and consonants count
+    print('Vowels: ' , vowel_count)
+    print('Consonants: ' ,  consonant_count)
+
+
+# call the function
+string = 'A quick brown fox jumps over the lazy dog'
+count_vowels_and_consonants(string)
